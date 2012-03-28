5ee6bbdd13cacf0c555092efe8feb31d2fc4e7cd
Doug Tyler <dougwt@gmail.com>
2012-03-27 18:00:47 -0700

    Changed spaces to tab characters to match the indentation style of the project.

--- plugins/time.py
+++ plugins/time.py
@@ -3,5 +3,5 @@ import datetime
 
 
 def main(*argv):
-    time = datetime.datetime.now().strftime('%I:%M %p')
-    return "According to my watch, the time is %s." % time
+	time = datetime.datetime.now().strftime('%I:%M %p')
+	return "According to my watch, the time is %s." % time
